14f0f4b9c0386fce9dcd4ce27f3c348cd33de58a
Miguel Santos <miguel@scentronix.com>
2021-05-31 17:02:28 +0100

    changes

--- mlapp/app.py
+++ mlapp/app.py
@@ -2,7 +2,7 @@ import typing
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from mlapp import dash_utils
+from auto-ml import dash_utils
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
